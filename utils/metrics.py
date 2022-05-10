f918917c3aefedab83a1f07af2c3aefddc929a52
Jin.Fish <1800016611@pku.edu.cn>
2022-05-11 03:31:26 +0800

    shuffle (forget to commit logging and metrics)

--- utils/metrics.py
+++ utils/metrics.py
@@ -58,7 +58,9 @@ class SqaMetric:
                     all_q_correct = False
             if all_q_correct: seq_cor += 1
 
-        print("Sequence Accuracy = %0.2f%% (%d/%d)" % (100.0 * seq_cor / seq_cnt, seq_cor, seq_cnt))
-        print("Answer Accuracy =   %0.2f%% (%d/%d)" % (100.0 * ans_cor / ans_cnt, ans_cor, ans_cnt))
+        # print("Sequence Accuracy = %0.2f%% (%d/%d)" % (100.0 * seq_cor / seq_cnt, seq_cor, seq_cnt))
+        # print("Answer Accuracy =   %0.2f%% (%d/%d)" % (100.0 * ans_cor / ans_cnt, ans_cor, ans_cnt))
+        # return [seq_cor, seq_cnt, ans_cor, ans_cnt]
 
-        return [seq_cor, seq_cnt, ans_cor, ans_cnt]
+        return ("Sequence Accuracy = %0.2f%% (%d/%d)" % (100.0 * seq_cor / seq_cnt, seq_cor, seq_cnt),
+                "Answer Accuracy =   %0.2f%% (%d/%d)" % (100.0 * ans_cor / ans_cnt, ans_cor, ans_cnt))
--- utils/util.py
+++ utils/util.py
@@ -9,6 +9,25 @@
 
 common tools
 """
+import logging
 
 
+def init_logging(root_log_path, debug=False):
+    fmt = logging.Formatter('%(asctime)s | %(levelname)s | %(filename)s | %(funcName)s | %(message)s',
+                            datefmt='%m-%d %H:%M:%S')
+    root_logger = logging.getLogger()
+    root_hdl = logging.FileHandler(root_log_path)
+    root_hdl.setFormatter(fmt)
+    root_logger.addHandler(root_hdl)
+    root_logger.setLevel(logging.INFO)
+    if debug:
+        debug_hdl = logging.StreamHandler()
+        debug_hdl.setFormatter(fmt)
+        root_logger.addHandler(debug_hdl)
+        root_logger.setLevel(logging.DEBUG)
 
+    # eval_logger = logging.getLogger('evaluation')
+    # eval_hdl = logging.FileHandler(eval_log_path)
+    # eval_hdl.setFormatter(fmt)
+    # eval_logger.addHandler(eval_hdl)
+    # eval_logger.setLevel(logging.INFO)
