95bb3084aa52f4ca37041fcfc5d687ea2170aca0
Jin.Fish <1800016611@pku.edu.cn>
2022-05-15 17:06:06 +0800

    [0515 finetune 3_small] use small tapas feat: load from pretrained

--- scripts/0515/3_small.sh
+++ scripts/0515/3_small.sh
@@ -7,6 +7,6 @@ echo "experiment:$EXP"
 nohup /home/v-xiaojin/miniconda3/envs/table/bin/python tapas-finetune.py --shuffle \
 --output_dir output/finetune/$EXP \
 --model_name google/tapas-small \
---batch_size 32 \
+--batch_size 64 \
 --epochs 20 \
 > output/finetune/$EXP.nohup 2>&1 & echo $! > output/finetune/$EXP.pid
