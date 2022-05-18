4651e11e29b70c95468add1ab4972a1fbbbd548c
Jin.Fish <1800016611@pku.edu.cn>
2022-05-19 00:19:41 +0800

    data preproc (latex), add tab struct modification

--- preproc/pipeline.py
+++ preproc/pipeline.py
@@ -15,6 +15,7 @@ import time
 import logging
 import argparse
 import collections
+import multiprocessing
 
 import bs4
 import pandas as pd
@@ -23,14 +24,12 @@ from pylatexenc.latex2text import LatexNodes2Text
 from utils.util import init_logging
 
 
-lg = logging.getLogger()
-
-
 class TableParser:
-    def __init__(self, debug=False):
+    def __init__(self, task_id, debug=False):
         self.latex_converter = LatexNodes2Text()
         self.debug = debug
         self.error_stat = list()
+        self.lg = logging.getLogger(f"TASK{task_id}")
 
     def parse_alttext(self, alttext: str):
         """convert alttext to plain ascii"""
@@ -45,6 +44,22 @@ class TableParser:
             math_alttext = math_node.get('alttext')
             math_alttext = self.parse_alttext(math_alttext)
             math_node.replace_with(math_alttext)
+
+    @staticmethod
+    def remove_thead(table: bs4.element.Tag):
+        body = table.find("tbody")
+        if body is None:
+            return
+        table_heads = table.find_all("thead")
+        if len(table_heads) > 0:  # thead exist
+            head = table_heads[0].extract()
+            head_rows = head.find_all("tr")
+            for row in head_rows[::-1]:
+                row = row.extract()
+                for th in row.find_all("th"):
+                    th.name = "td"
+                body.insert(0, row)
+        return table
 
     @staticmethod
     def valid_table(table: bs4.element.Tag):
@@ -78,7 +93,7 @@ class TableParser:
         with html_file.open("r", encoding="utf-8") as f:
             soup = bs(f, "html.parser")
         tables = soup.find_all("table", {'class': 'ltx_tabular'})
-        lg.info(f"----------------------- parsed {len(tables)} tables from {html_file}")
+        self.lg.info(f"----------------------- parsed {len(tables)} tables from {html_file}")
 
         # loop tables
         for i, table in enumerate(tables):
@@ -86,43 +101,50 @@ class TableParser:
                 valid_state = self.valid_table(table)
             except Exception as e:
                 self.error_stat.append(f"validation error")
-                lg.error(f"{html_file} table {i} validation error: {e}")
+                self.lg.error(f"{html_file} table {i} validation error: {e}")
                 continue
             if valid_state:
                 self.error_stat.append(valid_state)
-                lg.warning(f"{html_file} table {i} is not valid, because {valid_state}")
+                self.lg.warning(f"{html_file} table {i} is not valid, because {valid_state}")
                 continue
 
             try:
                 self.scrape_math(table)  # clean table content
             except Exception as e:
                 self.error_stat.append(f"scrape table math error")
-                lg.error(f"{html_file} table {i} scrape math error: {e}")
+                self.lg.error(f"{html_file} table {i} scrape math error: {e}, skip")
                 continue
 
             try:
                 caption = self.fetch_caption(table)  # get caption
             except Exception as e:
                 self.error_stat.append("caption unknown error")
-                lg.error(f"{html_file} table {i} is not valid, because {e}")
+                self.lg.error(f"{html_file} table {i} is not valid, because {e}, skip")
                 continue
+
             if not caption:
                 self.error_stat.append("no caption")
-                lg.warning(f"{html_file} table {i} has no caption, skip")
+                self.lg.warning(f"{html_file} table {i} has no caption, skip")
+                continue
+
+            table = self.remove_thead(table)  # remove thead (fixed missing header)
+            if not table:
+                self.error_stat.append("no tbody")
+                self.lg.error(f"{html_file} table {i} has no tbody, skip")
                 continue
 
             try:
-                df = pd.read_html(str(table))[0]  # todo missing header
+                df = pd.read_html(str(table))[0]
             except Exception as e:
                 self.error_stat.append("pandas error")
-                lg.error(f"{html_file} table {i} pandas error, because {e}")
+                self.lg.error(f"{html_file} table {i} pandas error, because {e}, skip")
                 continue
 
             csv_path = csv_dir / f"{html_file.name}_{i}.csv"
             df.to_csv(csv_path, index=False, header=False)
-            lg.info(f"convert table {i} to {csv_path}")
+            self.lg.info(f"convert table {i} to {csv_path}")
             table_res_list.append({'caption': caption, 'csv_path': csv_path})  # save metadata
-        lg.info(f"finish {html_file} in {time.time() - start_time}s")
+        self.lg.info(f"finish {html_file} in {time.time() - start_time}s")
         return table_res_list
 
     def fetch_caption(self, table: bs4.element.Tag):
@@ -146,12 +168,12 @@ class TableParser:
         #     success_list = [line.strip() for line in lines]
 
         html_file_list = list(html_dir.glob("*"))
-        lg.info(f"get {len(html_file_list)} html files")
+        self.lg.info(f"get {len(html_file_list)} html files")
 
         res_list = list()
         for idx, html_file in enumerate(html_file_list):
             if html_file.name in success_list:
-                lg.info(f"[INFO] {html_file} already converted")
+                self.lg.info(f"[INFO] {html_file} already converted")
                 continue
             csv_sub_dir = csv_dir / html_file.name
             if not csv_sub_dir.exists():
@@ -160,45 +182,58 @@ class TableParser:
             res_list.extend(res)
             with record_path.open("a", encoding="utf-8") as f:
                 f.write(f"{html_file.name}\n")
-            lg.info(f"convert {len(res_list)} csv files, {idx}/{len(html_file_list)} html files done")
+            self.lg.info(f"convert {len(res_list)} csv files, {idx}/{len(html_file_list)} html files done")
 
         df = pd.DataFrame(res_list)
         df.to_csv(csv_dir / "meta.csv", index=False)
 
     def report_error(self):
         counter = collections.Counter(self.error_stat)
-        lg.info(counter.most_common())
+        self.lg.info(counter.most_common())
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--html_dir", type=str, help="html dir", required=True)
     parser.add_argument("--csv_dir", type=str, help="csv dir", required=True)
-    parser.add_argument("--task_id", type=int, help="task id", required=True)
+    parser.add_argument("--proc_num", type=int, help="process num", default=1)
+    parser.add_argument("--task_id", nargs="+", type=int, help="task id", required=True)
+    parser.add_argument("--debug", action="store_true")
 
     args = parser.parse_args()
     return args
 
 
-def main():
-    args = get_args()
-    html_dir = pathlib.Path(args.html_dir)
-    csv_dir = pathlib.Path(args.csv_dir)
+def task(task_id: int, html_dir: pathlib.Path, csv_dir: pathlib.Path, debug: bool):
     if not csv_dir.exists():
         csv_dir.mkdir()
+        print(f"[TASK{task_id}] create csv dir {csv_dir}")
     log_path = csv_dir / "parse.log"
-    init_logging(root_log_path=log_path.absolute(), debug=True)
-
+    init_logging(root_log_path=log_path.absolute(), debug=debug, logger_name=f"TASK{task_id}")
+    lg = logging.getLogger(f"TASK{task_id}")
+    lg.info(f"start task {task_id}, html_dir: {html_dir}, csv_dir: {csv_dir}")
     start = time.time()
-    parser = TableParser(debug=True)
+    parser = TableParser(task_id=task_id, debug=debug)
     parser.pipeline(html_dir, csv_dir)
     lg.info(f"done in {time.time() - start}s")
     parser.report_error()
 
-    # html_path = pathlib.Path("../ar5iv_data/1712.04910v1")
-    # csv_path = pathlib.Path("../csv") / html_path.name
-    # parser.html2csv(html_path, csv_path)
+
+def multi_run():
+    args = get_args()
+    print(args)
+    html_dir = pathlib.Path(args.html_dir)
+    csv_dir = pathlib.Path(args.csv_dir)
+    p = multiprocessing.Pool(args.proc_num)
+    for i in args.task_id:
+        html_task_dir = html_dir / str(i)
+        csv_task_dir = csv_dir / str(i)
+        p.apply_async(task, args=(i, html_task_dir, csv_task_dir, args.debug))
+    print('Waiting for all subprocesses done...')
+    p.close()
+    p.join()
+    print('All subprocesses done.')
 
 
 if __name__ == "__main__":
-    main()
+    multi_run()
